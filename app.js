8d6e5043b64f74e1a78b468ca0fde0c9382b1d03
Abyul <kinsng1599@gmail.com>
2022-02-21 18:45:23 +0900

    :sparkles:feat: static 디렉토리 경로 지정 node 경우 static 디렉토리를 지정하지 않을 경우 html 측에 작성된 src 파일경로들은 'localhost:3000/파일경로' 형태로 불러 오게 됩니다.

--- app.js
+++ app.js
@@ -2,7 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+// static 디렉토리 경로 지정
+app.use(express.static('public'));
+
 app.get('/', function (req, res) {
+  console.log('localhost:3000');
+  res.sendFile(__dirname + '/public/main.html');
+});
+app.get('/main', function (req, res) {
+  console.log('localhost:3000/main');
   res.sendFile(__dirname + '/public/main.html');
 });
 
