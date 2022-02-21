e93577a1569466b47676ff1cc97d34a07a284abe
Abyul <kinsng1599@gmail.com>
2022-02-21 19:08:36 +0900

    :sparkles:feat: body-parser 추가 Express 4.x 이후부터는 body-parser 내장라이브러리로 변경됨

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
-// static 디렉토리 경로 지정
+// middleware
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
+// routing
 app.get('/', function (req, res) {
   console.log('localhost:3000');
   res.sendFile(__dirname + '/public/main.html');
@@ -13,7 +16,10 @@ app.get('/main', function (req, res) {
   console.log('localhost:3000/main');
   res.sendFile(__dirname + '/public/main.html');
 });
-
+app.post('/email_post', function (req, res) {
+  console.log('localhost:3000/email_post');
+  res.send('<h1>welcome ' + req.body.email + '!</h1>');
+});
 app.listen(port, () => {
   console.log('start, express server on port 3000');
 });
