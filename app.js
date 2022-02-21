69b9c946c037847f367fccf34e6fb96ac69222a2
Abyul <kinsng1599@gmail.com>
2022-02-21 18:32:06 +0900

    :sparkles:feat: GET '/' main.html 연결

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/public/main.html');
+});
+
 app.listen(port, () => {
   console.log('start, express server on port 3000');
 });
