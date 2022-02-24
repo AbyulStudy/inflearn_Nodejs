bcb073a8f1a96ee9f9cf87b16e794bcf820eca17
Abyul <kinsng1599@gmail.com>
2022-02-24 22:40:07 +0900

    :sparkles:feat: error routing 추가

--- router/index.js
+++ router/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const path = require('path');
 const main = require('./main');
 const email = require('./email');
-
+const error = require('./error/error');
 // url routing
 router.get('/', function (req, res, next) {
   console.log('127.0.0.1:3000');
@@ -11,5 +11,6 @@ router.get('/', function (req, res, next) {
 });
 router.use('/main', main);
 router.use('/email', email);
+router.use('./error', error);
 
 module.exports = router;
