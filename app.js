ac144e892c20742c62ca40adde18210abbd67e31
Abyul <kinsng1599@gmail.com>
2022-02-24 21:46:47 +0900

    :sparkles:feat: mysql2 prepareStatemens 를 이용한 2가지 방법으로 데이터 처리 및 전송 로직 작성

--- app.js
+++ app.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const mysql = require('mysql2');
+
+// mysql connection
+const connection = (mysql.createConnection = mysql.createConnection({
+  host: 'localhost',
+  port: '3306',
+  user: 'root',
+  password: 'root',
+  database: 'jsman',
+}));
+
+connection.connect();
 
 // middleware
 app.use(express.static('public'));
@@ -24,10 +35,36 @@ app.post('/email_post', function (req, res) {
 });
 app.post('/ajax_send_email', function (req, res) {
   console.log('localhost:3000/ajax_send_email');
-  console.log('req.body : ', req.body);
-  const responseData = { result: 'ok', email: req.body.email };
-  res.json(responseData);
+  const email = req.body.email;
+  let responseData = {};
+  // const query = connection.prepare('select name from user where email = ?', (err, statement) => {
+  //   statement.execute([email], (err, rows) => {
+  //     if (err) throw err;
+  //     if (rows[0]) {
+  //       responseData.result = 'ok';
+  //       responseData.name = rows[0].name;
+  //     } else {
+  //       console.log('none : ' + rows[0]);
+  //       responseData.name = '';
+  //     }
+  //     res.json(responseData);
+  //   });
+  // });
+  const query = connection.execute('select name from user where email =?', [email], (err, rows) => {
+    if (err) throw err;
+    if (rows[0]) {
+      responseData.result = 'ok';
+      responseData.name = rows[0].name;
+    } else {
+      console.log('none : ' + rows[0]);
+      responseData.name = '';
+    }
+    res.json(responseData);
+  });
 });
+
+const port = 3000;
+
 app.listen(port, () => {
   console.log('start, express server on port 3000');
 });
