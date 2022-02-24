720fb4b24036750bc904d7e7ef8a152eeb4607d5
Abyul <kinsng1599@gmail.com>
2022-02-24 22:38:47 +0900

    :sparkles:feat: 404, 500 에러 핸들링 코드 작성

--- app.js
+++ app.js
@@ -13,7 +13,17 @@ const router = require('./router/index');
 
 app.use(router);
 
+// error (404, 500)
+app.use((req, res, next) => {
+  res.status(404).send('Sorry cant find taht!');
+});
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 const port = 3000;
+
 app.listen(port, () => {
   console.log('start, express server on port 3000');
 });
