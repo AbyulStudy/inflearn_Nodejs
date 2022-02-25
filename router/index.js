9c3481bd8f77882da3cd38213ec37c038220ea5c
Abyul <kinsng1599@gmail.com>
2022-02-25 23:53:21 +0900

    :sparkles:feat: join routing 추가

--- router/index.js
+++ router/index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const main = require('./main/main');
 const email = require('./email/email');
 const error = require('./error/error');
-
+const join = require('./join/index');
 // url routing
 router.get('/', function (req, res, next) {
   console.log('127.0.0.1:3000');
@@ -13,5 +13,6 @@ router.get('/', function (req, res, next) {
 router.use('/main', main);
 router.use('/email', email);
 router.use('/error', error);
+router.use('/join', join);
 
 module.exports = router;
