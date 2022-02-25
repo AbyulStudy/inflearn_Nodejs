7e221f7cffb84c8487c519171d725169b8331b11
Abyul <kinsng1599@gmail.com>
2022-02-25 23:47:41 +0900

    :sparkles:feat: error 경로 출력

--- app.js
+++ app.js
@@ -18,8 +18,9 @@ app.use((req, res, next) => {
   res.status(404).send('Sorry cant find taht!');
 });
 app.use((err, req, res, next) => {
+  console.log(err.route);
   console.error(err.stack);
-  res.status(500).send('Something broke!');
+  res.status(500).send('Something broke!<br>' + err);
 });
 
 const port = 3000;
