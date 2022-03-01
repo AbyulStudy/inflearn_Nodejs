4c22110fbe248d0a4e38624d79280868ad2b4aeb
Abyul <kinsng1599@gmail.com>
2022-03-02 02:15:42 +0900

    :sparkles:feat: [ connection ] select 문을 이용한 예제 코드 - 2

--- router/mysql2/promiseWrapper/promise.js
+++ router/mysql2/promiseWrapper/promise.js
@@ -29,4 +29,32 @@ router.get('/promise/conn', (req, res, next) => {
   });
 });
 
+router.get('/promise/conn2', (req, res, next) => {
+  const conn = mysql.createConnection({
+    host: 'localhost',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'jsman',
+  });
+  const name = 'devbyul';
+  conn
+    .then((connection) => {
+      const res = connection.execute('select * from user where name = ?', [
+        name,
+      ]);
+      connection.end();
+      return res;
+    })
+    .then(([results, fields, err]) => {
+      if (err) throw err;
+      res.json(results);
+      console.log(err);
+    })
+    .catch((err) => {
+      err.route = '[Error Route] : /promise/conn {GET}';
+      next(err);
+    });
+});
+
 module.exports = router;
