2cdf1e96eb03b2a32ae79064355b95a065067410
Abyul <kinsng1599@gmail.com>
2022-03-02 02:07:09 +0900

    :sparkles:feat: [ mysql2 ] Promise Wrapper 예시코드 routing 추가

--- router/index.js
+++ router/index.js
@@ -5,14 +5,20 @@ const main = require('./main/main');
 const email = require('./email/email');
 const error = require('./error/error');
 const join = require('./join/index');
+const promise = require('./mysql2/promiseWrapper/promise');
+const asyncAwait = require('./mysql2/promiseWrapper/asyncAwait');
+
 // url routing
 router.get('/', function (req, res, next) {
   console.log('127.0.0.1:3000');
   res.sendFile(path.join(__dirname, '../public/main.html'));
 });
+
 router.use('/main', main);
 router.use('/email', email);
 router.use('/error', error);
 router.use('/join', join);
+router.use('/mysql2', promise);
+router.use('/mysql2', asyncAwait);
 
 module.exports = router;
